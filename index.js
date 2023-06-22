edade503801b18ffce5ba431cca9d822c4f5731a
jake-knox- <jakeknox11@hotmail.co.uk>
2023-06-23 00:14:45 +0100

    a few different versions of visualisation, not much experimentation so far. Some look quite good though.

--- index.js
+++ index.js
@@ -51,8 +51,255 @@ function renderVisualizer() {
   requestAnimationFrame(renderVisualizer);
 }
 
+// Function to render the visualizer
+function renderVisualizer2() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+    // Get the current frequency data
+    analyser.getByteFrequencyData(dataArray);
+  
+    // Calculate the radius of the circle
+    const circleRadius = Math.min(canvas.width, canvas.height) / 10;
+  
+    // Calculate the angle between bars
+    const angleIncrement = (2 * Math.PI) / bufferLength;
+  
+    // Draw the bars
+    for (let i = 0; i < bufferLength; i++) {
+      const barHeight = dataArray[i] * (canvas.height / 255);
+  
+      // Calculate the position of the bar
+      const angle = i * angleIncrement;
+      const x = canvas.width / 2 + Math.cos(angle) * circleRadius;
+      const y = canvas.height / 2 + Math.sin(angle) * circleRadius;
+  
+      // Calculate the size of the bar
+      const barWidth = barHeight / 2;
+  
+      ctx.save();
+  
+      // Rotate the canvas to align the bars with the circle
+      ctx.translate(x, y);
+      ctx.rotate(angle);
+  
+      ctx.fillStyle = 'rgb(' + (barHeight + 100) + ', 50, 50)';
+      ctx.fillRect(0, -barWidth / 2, barHeight, barWidth);
+  
+      ctx.restore();
+    }
+  
+    // Call the render function in the next frame
+    requestAnimationFrame(renderVisualizer2);
+}
+  
+// Function to render the visualizer
+function renderVisualizer3() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+    // Get the current time domain data
+    analyser.getByteTimeDomainData(dataArray);
+  
+    // Calculate bar width
+    const barWidth = canvas.width / bufferLength;
+  
+    // Draw the bars based on pitch
+    for (let i = 0; i < bufferLength; i++) {
+      // Convert the audio data from the range of 0-255 to -1 to 1
+      const sample = (dataArray[i] - 128) / 128;
+  
+      // Calculate the pitch (frequency) using the index
+      const pitch = (i / bufferLength) * audioContext.sampleRate;
+  
+      // Calculate the bar height based on the pitch
+      const barHeight = Math.abs(sample) * canvas.height;
+  
+      const x = i * barWidth;
+      const y = canvas.height - barHeight;
+  
+      ctx.fillStyle = 'rgb(50, ' + (barHeight + 100) + ', 50)';
+      ctx.fillRect(x, y, barWidth, barHeight);
+    }
+  
+    // Call the render function in the next frame
+    requestAnimationFrame(renderVisualizer3);
+}
+  
+// Number of bars to use for the moving average
+const smoothingFactor = 10;
+let smoothedDataArray = new Array(bufferLength).fill(0);
+
+// Function to render the visualizer
+function renderVisualizer4() {
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Get the current time domain data
+  analyser.getByteTimeDomainData(dataArray);
+
+  // Apply moving average smoothing
+  for (let i = 0; i < bufferLength; i++) {
+    smoothedDataArray[i] += (dataArray[i] - 128) / 128;
+    if (i >= smoothingFactor) {
+      smoothedDataArray[i] -= (dataArray[i - smoothingFactor] - 128) / 128;
+    }
+    smoothedDataArray[i] /= smoothingFactor;
+  }
+
+  // Calculate bar width
+  const barWidth = canvas.width / bufferLength;
+
+  // Draw the bars based on the smoothed data
+  for (let i = 0; i < bufferLength; i++) {
+    // Calculate the pitch (frequency) using the index
+    const pitch = (i / bufferLength) * audioContext.sampleRate;
+
+    // Calculate the bar height based on the pitch and smoothed data
+    const barHeight = Math.abs(smoothedDataArray[i]) * canvas.height;
+
+    const x = i * barWidth;
+    const y = canvas.height - barHeight;
+
+    ctx.fillStyle = 'rgb(50, ' + (barHeight * 50) + ', 50)';
+    ctx.fillRect(x, y, barWidth, barHeight);
+  }
+
+  // Call the render function in the next frame
+  requestAnimationFrame(renderVisualizer4);
+}
+
+
+
+ // Helper function to convert HSB color to RGB
+ function hsbToRgb(h, s, br) {
+    h /= 60;
+    s /= 100;
+    br /= 100;
+  
+
+    const i = Math.floor(h);
+    const f = h - i;
+    const p = br * (1 - s);
+    const q = br * (1 - s * f);
+    const t = br * (1 - s * (1 - f));
+  
+    let r, g, b;
+    if (i === 0) {
+      [r, g, b] = [br, t, p];
+    } else if (i === 1) {
+      [r, g, b] = [q, br, p];
+    } else if (i === 2) {
+      [r, g, b] = [p, br, t];
+    } else if (i === 3) {
+      [r, g, b] = [p, q, br];
+    } else if (i === 4) {
+      [r, g, b] = [t, p, br];
+    } else {
+      [r, g, b] = [br, p, q];
+    }
+  
+    return [
+      Math.round(r * 255),
+      Math.round(g * 255),
+      Math.round(b * 255)
+    ];
+  }
+
+// Function to render the visualizer
+function renderVisualizer5() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+    // Get the current frequency data
+    analyser.getByteFrequencyData(dataArray);
+  
+    // Calculate bar width
+    const barWidth = canvas.width / bufferLength;
+  
+    // Define initial color values
+    let hue = 0;
+    let saturation = 100;
+  
+    // Draw the bars
+    for (let i = 0; i < bufferLength; i++) {
+      const barHeight = dataArray[i] * (canvas.height / 255);
+  
+      // Adjust brightness based on bar height
+      const brightness = Math.min(100 + barHeight, 100);
+  
+      // Convert HSB color to RGB
+      const rgbColor = hsbToRgb(hue, saturation, brightness);
+  
+      const x = i * barWidth;
+      const y = canvas.height - barHeight;
+  
+      ctx.fillStyle = 'rgb(' + rgbColor.join(', ') + ')';
+      ctx.fillRect(x, y, barWidth, barHeight);
+  
+      // Update color values
+      hue = (hue + 1) % 360;
+    }
+  
+    // Call the render function in the next frame
+    requestAnimationFrame(renderVisualizer5);
+}
+  
+ // Set the desired number of bars
+const numBars = 30;
+const bufferLength2 = numBars;
+
+// Function to render the visualizer
+function renderVisualizer6() {
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Get the current frequency data
+  analyser.getByteFrequencyData(dataArray);
+
+  // Calculate bar width
+  const barWidth = canvas.width / numBars;
+
+  // Define initial color values
+  let hue = 0;
+  let saturation = 100;
+
+  // Draw the bars
+  for (let i = 0; i < numBars; i++) {
+    const dataIndex = Math.floor(i * (bufferLength2 / numBars));
+    const barHeight = dataArray[dataIndex] * (canvas.height / 255);
+
+    // Adjust brightness based on bar height
+    const brightness = Math.min(100 + barHeight, 100);
+
+    // Convert HSB color to RGB
+    const rgbColor = hsbToRgb(hue, saturation, brightness);
+
+    const x = i * barWidth;
+    const y = canvas.height - barHeight;
+
+    ctx.fillStyle = 'rgb(' + rgbColor.join(', ') + ')';
+    ctx.fillRect(x, y, barWidth, barHeight);
+
+    // Update color values
+    hue = (hue + 1) % 360;
+  }
+
+  // Call the render function in the next frame
+  requestAnimationFrame(renderVisualizer6);
+}
+
+  
+
+
+
 // Start the audio playback
 audioElement.play();
 
 // Start rendering the visualizer
-renderVisualizer();
+// renderVisualizer();
+// renderVisualizer2();
+// renderVisualizer3();
+// renderVisualizer4();
+// renderVisualizer5();
+renderVisualizer6();
